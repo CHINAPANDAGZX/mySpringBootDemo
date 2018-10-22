39c89490a3fbe9a3f5580ca0c2260a4ed05bbfd2
gzx <493004728@qq.com>
2018-10-22 15:54:41 +0800

    2018年10月22日15:51:05-gzx 1、登录过滤器的增加

--- src/main/java/com/example/demo/common/SessionFilter.java
+++ src/main/java/com/example/demo/common/SessionFilter.java
@@ -1,25 +1,14 @@
 package com.example.demo.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.xmrbi.segs.entity.security.SystemUser;
-//import com.xmrbi.segs.jwt.Audience;
-//import com.xmrbi.segs.jwt.JwtHelper;
-//import com.xmrbi.segs.service.security.SystemUserService;
-//import io.jsonwebtoken.Claims;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @Title: SessionFilter.java   
@@ -29,13 +18,17 @@ import java.util.Map;
  * @date 2017年10月11日 上午8:57:02   
  * @version V1.0     
  */
-@WebFilter(urlPatterns="/*",asyncSupported=true)
-public class SessionFilter implements Filter{
+//@WebFilter(urlPatterns="/*",asyncSupported=true)
+public class SessionFilter implements Filter {
 	
 //	@Autowired
 //    private Audience audienceEntity;
 //	@Autowired
 //	private SystemUserService systemUserService;
+	//标示符：表示当前用户未登录(可根据自己项目需要改为json样式)
+	String NO_LOGIN = "您还未登录";
+	//不需要登录就可以访问的路径(比如:注册登录等)
+	String[] includeUrls = new String[]{"/login","register"};
 	/**
      * 封装，不需要过滤的list列表
      */
@@ -48,67 +41,37 @@ public class SessionFilter implements Filter{
 	}
 
 	@Override
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
-		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-        //非法字符过滤器
-        httpRequest = new MHttpServletRequest(httpRequest);
-        String url = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
-        if (isInclude(url)){
-            chain.doFilter(httpRequest, httpResponse);
-            return;
-        }
-//        else {
-//        	//如果是移动端请求，则验证token
-//    		String auth = httpRequest.getHeader("Authorization");
-//    		if(!StringUtils.isEmpty(auth)) {
-//        		if (JwtHelper.parseJWT(auth, audienceEntity.getBase64Secret()) != null) {
-//        			Claims claim = JwtHelper.parseJWT(auth, this.audienceEntity.getBase64Secret());
-//        			String userId = (String)claim.get("userId");
-//        			SystemUser u = this.systemUserService.findUserById(userId);
-//        			if(u!=null&&u.getIsValid()){
-//	                    chain.doFilter(httpRequest, httpResponse);
-//	                    return;
-//        			}else{
-//        				httpResponse.setCharacterEncoding("UTF-8");
-//    	                httpResponse.setContentType("application/json; charset=utf-8");
-//    	                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//    	                ObjectMapper mapper = new ObjectMapper();
-//    	                Map<String, Object> resultMap = new LinkedHashMap<String,Object>();
-//    	                resultMap.put("code", -2);
-//    	                resultMap.put("msg", u==null?"用户不存在":"用户已失效");
-//    	                resultMap.put("data", null);
-//    	                httpResponse.getWriter().write(mapper.writeValueAsString(resultMap));
-//        			}
-//        		}else{
-//	                httpResponse.setCharacterEncoding("UTF-8");
-//	                httpResponse.setContentType("application/json; charset=utf-8");
-//	                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//	                ObjectMapper mapper = new ObjectMapper();
-//	                Map<String, Object> resultMap = new LinkedHashMap<String,Object>();
-//	                resultMap.put("code", -1);
-//	                resultMap.put("msg", "Token 无效");
-//	                resultMap.put("data", null);
-//	                httpResponse.getWriter().write(mapper.writeValueAsString(resultMap));
-//                }
-//    		}else {
-//	        	//否则web端验证session
-//	            HttpSession session = httpRequest.getSession();
-//	            if (session.getAttribute("SYS_USER") != null){
-//	        		chain.doFilter(httpRequest, httpResponse);
-//	                return;
-//	            } else {
-//	            	if(url.indexOf("/main")>-1) {
-//	            		//跳转至登录页面
-//	            		httpResponse.sendRedirect(httpRequest.getContextPath()+"/login.html");
-//	            	}else {
-//	            		// session不存在 准备跳转失败
-//	            		throw new ServletException("会话不存在或已经过期");
-//	            	}
-//	            }
-//    		}
-//        }
+
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		HttpSession session = request.getSession(false);
+		String uri = request.getRequestURI();
+
+		System.out.println("过滤URI:"+uri);
+		//是否需要过滤
+		boolean needFilter = isNeedFilter(uri);
+
+		if(!needFilter){
+			filterChain.doFilter(servletRequest,servletResponse);//不需要过滤，直接传递给下一个过滤器
+		}else{
+			if(session!=null&&session.getAttribute("user") != null){
+				// System.out.println("user:"+session.getAttribute("user"));
+				filterChain.doFilter(request, response);
+			}else{
+				String requestType = request.getHeader("X-Requested-With");
+				//判断是否是ajax请求
+				if(requestType!=null && "XMLHttpRequest".equals(requestType)){
+					response.getWriter().write(this.NO_LOGIN);
+				}else{
+					//跳转到登录页
+					response.sendRedirect(request.getContextPath()+"/login.html");
+				}
+			}
+
+		}
+
 	}
 
 	@Override
@@ -116,7 +79,8 @@ public class SessionFilter implements Filter{
 		System.out.println("======过滤器初始化======");
 		patterns.add("/");
 		patterns.add("/login.html");
-		patterns.add("/login");
+		patterns.add("/login");//登录页面的地址
+		patterns.add("/layui/*");//样式的地址
 		patterns.add("/static/*");
 		patterns.add("/login/validate");
 		patterns.add("/login/validate.html");
@@ -126,7 +90,7 @@ public class SessionFilter implements Filter{
 		patterns.add("/essi/*");
 		patterns.add("/annex/get-file-stream");
 		patterns.add("/mobile/login/*");
-		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, filterConfig.getServletContext());  
+//		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, filterConfig.getServletContext());
 	}
 
 	
@@ -135,16 +99,16 @@ public class SessionFilter implements Filter{
      * @param url
      * @return
      */
-    private boolean isInclude(String url) {
-    	if (patterns == null || patterns.size() <= 0) {  
-            return false;  
-        }  
-        for (String ex : patterns) {  
-        	url = url.trim();  
-            ex = ex.trim();  
-            if (url.toLowerCase().matches(ex.toLowerCase().replace("*",".*")))  
-                return true;  
-        }  
-        return false;
+    private boolean isNeedFilter(String url) {
+    	if (patterns == null || patterns.size() <= 0) {
+            return true;
+        }
+        for (String ex : patterns) {
+        	url = url.trim();
+            ex = ex.trim();
+            if (url.toLowerCase().matches(ex.toLowerCase().replace("*",".*")))
+                return false;
+        }
+        return true;
     }
 }
